1a30a923791594abb63e304e3c2c6d2c4b2d6ef6
ivynguyen728 <vnguyen280799@gmail.com>
2025-06-05 12:00:04 -0700

    Delete Testing.r

--- Scripts/Testing.r
+++
@@ -1,7 +0,0 @@
-ins_df = read.csv("Data/insurance.csv")
-lm_model = lm(charges ~ age + bmi + children, data = ins_df)
-par(mfrow = c(2, 2))  # optional: layout in a grid
-plot(lm_model, which = 1)  # Residuals vs Fitted
-plot(lm_model, which = 2)  # Normal Q-Q
-plot(lm_model, which = 3)  # Scale-Location
-plot(lm_model, which = 5)  # Residuals vs Leverage
